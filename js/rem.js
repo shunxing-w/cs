46f2f9e5de7422be05ab2171698fd11360fe4aa3
shunxing-w <shunxing_w@163.com>
2017-04-22 14:21:07 +0800

    Update rem.js

--- js/rem.js
+++ js/rem.js
@@ -1,6 +1,25 @@
 /**
  * Created by yd on 2016/12/19.
  */
+(function() {
+    if (typeof WeixinJSBridge == "object" && typeof WeixinJSBridge.invoke == "function") {
+        handleFontSize();
+    } else {
+        if (document.addEventListener) {
+            document.addEventListener("WeixinJSBridgeReady", handleFontSize, false);
+        } else if (document.attachEvent) {
+            document.attachEvent("WeixinJSBridgeReady", handleFontSize);
+            document.attachEvent("onWeixinJSBridgeReady", handleFontSize);  }
+    }
+    function handleFontSize() {
+        // 设置网页字体为默认大小
+        WeixinJSBridge.invoke('setFontSizeCallback', { 'fontSize' : 0 });
+        // 重写设置网页字体大小的事件
+        WeixinJSBridge.on('menu:setfont', function() {
+            WeixinJSBridge.invoke('setFontSizeCallback', { 'fontSize' : 0 });
+        });
+    }
+})();
 !function(e, t) {
     function i() {
         o = 1, e.devicePixelRatioValue = o, s = 1 / o;
